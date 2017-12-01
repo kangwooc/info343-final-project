f68d0c4a45121eaf98deec4d51ee03e7199ddfbb
Kangwoo <kangwooc@D-140-142-244-166.dhcp4.washington.edu>
2017-12-01 12:24:51 -0800

    add firebase key and Constant

--- trivia/src/index.js
+++ trivia/src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-
+var config = {
+    apiKey: "AIzaSyCuJ_qu84ygtmZ5vNOlGFsCxsRyBSqBGWg",
+    authDomain: "info343-final-ff532.firebaseapp.com",
+    databaseURL: "https://info343-final-ff532.firebaseio.com",
+    projectId: "info343-final-ff532",
+    storageBucket: "info343-final-ff532.appspot.com",
+    messagingSenderId: "493150428563"
+  };
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
