2771d1ed8e5718835c0cd9feb82e7106521893ef
witchperson <kangwooc@gmail.com>
2017-12-07 03:11:12 -0800

    fix the error from score

--- trivia/src/components/ResultPage.jsx
+++ trivia/src/components/ResultPage.jsx
@@ -1,5 +1,8 @@
 import React, {Component} from 'react';
-
+import constant from './Constants';
+import firebase from "firebase/app";
+import 'firebase/auth';
+import "firebase/database";
 
 export default class ResultPage extends Component {
 
@@ -10,22 +13,42 @@ export default class ResultPage extends Component {
         };
     }
 
-    home() {
-        //direct back to mainpage
-        //this.props.history.push("/");
+    componentDidMount(){
+        this.authUnsub = firebase.auth().onAuthStateChanged(user => {
+            this.setState({displayName: user.displayName});
+        });
+    }
+
+    componentWillMount(){
+        this.getScore();
+    }
+
+    getScore(){
+        var dateobj= new Date() ;
+        var month = dateobj.getMonth() + 1;
+        var day = dateobj.getDate() ;
+        var year = dateobj.getFullYear();
+        console.log(console.log(this.state.displayName));
+        // let scoreRef = firebase.database().ref(month+"-"+day+"-"+year).child(displayName).on("value", (snapshot)=>{
+            
+        // });
+    }
+
+    clickHome() {
+        this.props.history.push("/mainpage");
     }
 
     render() {
         return (
             <div>
-                <div className="container result">
+                <div className="container" id = "result">
                     <h1>Congratulations!</h1>
                     <h4>You answered {this.state.correct} out of 10 questions correctly!</h4>
-                    <img src="../img/goodjob.jpg"/>
+                    <img src="../goodjob.jpg" alt = "good job" height="42" width="42"/>
                     <h4>Come back tomorrow and try your luck again.</h4>
                     <button
                         className="btn btn-info" 
-                        onClick={() => this.home()}>
+                        onClick={() => this.clickHome()}>
                         Home
                     </button>
                 </div>
